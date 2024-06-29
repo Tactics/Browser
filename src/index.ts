a99ac8d42b909ed9f4643bcb6969419f7f09949c
Gert van den Buijs <gertvandenbuijs@hotmail.com>
2024-06-29 15:09:14 +0200

    setup

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export type { prefersReducedMotion, prefersContrast, prefersReducedTransparency, prefersColorSchema, invertedColors } from "@/accessibility/preferences";
+export { BrowserAccessibilityPreferences } from "@/accessibility/preferences";
+export { useBrowserAccessibilityPreferences } from "@/accessibility/preferences";
